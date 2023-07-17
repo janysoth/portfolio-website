9a940c67eba6f440751d291932852f28c9467886
Jonny Vorn Soth <janysoth@gmail.comm>
2023-07-17 00:05:19 -0500

    Route PortfolioView.vue for the header.

--- portfolio-website/src/App.vue
+++ portfolio-website/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/porfolio-view">My Portfolio</router-link> |
+      <router-link to="/portfolio-view">My Portfolio</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
