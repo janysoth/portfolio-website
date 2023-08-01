25b6ce813e9d5c9b1b0970df488eba459f4648d1
Jonny Soth <janysoth@gmail.com>
2023-08-01 05:19:59 -0500

    Remove :type from the button.

--- portfolio-website/src/components/ui-library/ButtonComp.vue
+++ portfolio-website/src/components/ui-library/ButtonComp.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <button
-      :type="type"
-      class="px-6 py-2 font-semibold text-white bg-blue-500 rounded-md hover:opacity-95 focus:outline-none"
+      class="px-6 py-2 font-semibold text-white bg-blue-500 rounded-md hover:opacity-95 focus:outline-none mt-2 mb-2"
       @click="displayMessage"
     >
       <slot>Submit</slot>
     </button>
-    <p v-if="message">{{ message }}</p>
+    <p>{{ message }}</p>
   </div>
 </template>
 
@@ -36,7 +35,5 @@ const message = ref("");
 // Function to display the message
 const displayMessage = () => {
   message.value = "Button clicked!";
-  // Replace the above line with your custom logic to display the message
-  // For example, you can show an alert like this: alert('Button clicked!');
 };
 </script>
