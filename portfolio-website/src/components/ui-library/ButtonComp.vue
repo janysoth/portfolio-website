15db2f25054310d432b2bd062f0a4934e9bb9e4d
Jonny Soth <janysoth@gmail.com>
2023-08-01 05:18:25 -0500

    Create a function to display message when the user click the button.

--- portfolio-website/src/components/ui-library/ButtonComp.vue
+++ portfolio-website/src/components/ui-library/ButtonComp.vue
@@ -1,10 +1,14 @@
 <template>
-  <button
-    :type="type"
-    class="px-6 py-2 font-semibold text-white bg-blue-500 rounded-md hover:opacity-95 focus:outline-none"
-  >
-    <slot>Submit</slot>
-  </button>
+  <div>
+    <button
+      :type="type"
+      class="px-6 py-2 font-semibold text-white bg-blue-500 rounded-md hover:opacity-95 focus:outline-none"
+      @click="displayMessage"
+    >
+      <slot>Submit</slot>
+    </button>
+    <p v-if="message">{{ message }}</p>
+  </div>
 </template>
 
 <!-- <script>
@@ -23,5 +27,16 @@ export default {
 };
 </script> -->
 
-<script setup></script>
-<style></style>
+<script setup>
+import { ref } from "vue";
+
+// Define a ref to track the message state
+const message = ref("");
+
+// Function to display the message
+const displayMessage = () => {
+  message.value = "Button clicked!";
+  // Replace the above line with your custom logic to display the message
+  // For example, you can show an alert like this: alert('Button clicked!');
+};
+</script>
