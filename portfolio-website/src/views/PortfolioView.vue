b41b0f7605312b12440636dc528bc6c764cf188f
Jonny Soth <janysoth@gmail.com>
2023-08-08 05:27:02 -0500

    Add a clear message button.

--- portfolio-website/src/views/PortfolioView.vue
+++ portfolio-website/src/views/PortfolioView.vue
@@ -2,8 +2,19 @@
   <div class="portfolio-view">
     <h1>This is a Portfolio Page</h1>
     <p>I'm currently learning JavaScript and Vue 3.</p>
-    <ButtonComp :button-text="buttonText" @button-clicked="handleButtonClick" />
+    <ButtonComp
+      v-if="!message"
+      :button-text="buttonText"
+      @button-clicked="handleButtonClick"
+    />
     <p>{{ message }}</p>
+    <button
+      v-if="message"
+      class="px-4 py-2 font-semibold text-white bg-red-500 rounded-md hover:opacity-95 focus:outline-none mt-2"
+      @click="removeMessage"
+    >
+      Clear Message
+    </button>
   </div>
 </template>
 
@@ -24,6 +35,10 @@ export default {
     handleButtonClick() {
       // Update the message when the button is clicked
       this.message = "You have clicked a button!";
+    },
+
+    removeMessage() {
+      this.message = ""; // Clear the message
     },
   },
 };
