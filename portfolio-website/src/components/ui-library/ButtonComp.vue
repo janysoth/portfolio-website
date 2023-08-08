3a7a282c6f2d079126d48de653d940308a1e9111
Jonny Soth <janysoth@gmail.com>
2023-08-08 05:16:08 -0500

    Add a handleButtonClick function to dsiplay a message when the user click the button.

--- portfolio-website/src/components/ui-library/ButtonComp.vue
+++ portfolio-website/src/components/ui-library/ButtonComp.vue
@@ -2,13 +2,35 @@
   <div>
     <button
       class="px-6 py-2 font-semibold text-white bg-blue-500 rounded-md hover:opacity-95 focus:outline-none mt-2 mb-2"
-      @click="displayMessage"
+      @click="onButtonClick"
     >
-      <slot>Submit</slot>
+      {{ buttonText }}
     </button>
     <p>{{ message }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "ButtonComp",
+  props: {
+    type: {
+      type: String,
+      default: "Submit",
+    },
+    buttonText: {
+      type: String,
+      default: "Click Me",
+      required: true,
+    },
+  },
+  methods: {
+    onButtonClick() {
+      this.$emit("button-clicked");
+    },
+  },
+};
+</script>
 
 <!-- <script>
 export default {
@@ -26,7 +48,7 @@ export default {
 };
 </script> -->
 
-<script setup>
+<!-- <script setup>
 import { ref } from "vue";
 
 // Define a ref to track the message state
@@ -36,4 +58,4 @@ const message = ref("");
 const displayMessage = () => {
   message.value = "Button clicked!";
 };
-</script>
+</script> -->
--- portfolio-website/src/views/PortfolioView.vue
+++ portfolio-website/src/views/PortfolioView.vue
@@ -2,10 +2,29 @@
   <div class="portfolio-view">
     <h1>This is a Portfolio Page</h1>
     <p>I'm currently learning JavaScript and Vue 3.</p>
-    <ButtonComp>Click Me</ButtonComp>
+    <ButtonComp :button-text="buttonText" @button-clicked="handleButtonClick" />
+    <p>{{ message }}</p>
   </div>
 </template>
 
-<script setup>
+<script>
 import ButtonComp from "../../src/components/ui-library/ButtonComp.vue";
+
+export default {
+  components: {
+    ButtonComp,
+  },
+  data() {
+    return {
+      buttonText: "Click Here",
+      message: "",
+    };
+  },
+  methods: {
+    handleButtonClick() {
+      // Update the message when the button is clicked
+      this.message = "You have clicked a button!";
+    },
+  },
+};
 </script>
