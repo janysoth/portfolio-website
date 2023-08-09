f39f62cceb7ea651f2dca5b68a3686547dbf21d7
Jonny Soth <janysoth@gmail.com>
2023-08-09 05:05:52 -0500

    Modify the clear message button.

--- portfolio-website/src/views/PortfolioView.vue
+++ portfolio-website/src/views/PortfolioView.vue
@@ -4,17 +4,22 @@
     <p>I'm currently learning JavaScript and Vue 3.</p>
     <ButtonComp
       v-if="!message"
-      :button-text="buttonText"
+      button-text="Click Me"
       @button-clicked="handleButtonClick"
     />
     <p>{{ message }}</p>
-    <button
+    <ButtonComp
+      button-text="Clear Message"
+      v-if="message"
+      @button-clicked="removeMessage"
+    />
+    <!-- <button
       v-if="message"
       class="px-4 py-2 font-semibold text-white bg-red-500 rounded-md hover:opacity-95 focus:outline-none mt-2"
       @click="removeMessage"
     >
       Clear Message
-    </button>
+    </button> -->
   </div>
 </template>
 
@@ -27,7 +32,6 @@ export default {
   },
   data() {
     return {
-      buttonText: "Click Here",
       message: "",
     };
   },
