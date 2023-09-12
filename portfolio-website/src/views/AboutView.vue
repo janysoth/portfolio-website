4839e23734ded39f9234048099cf32e47cc2f1d7
Jonny Soth <janysoth@gmail.com>
2023-09-12 05:03:21 -0500

    Create a simple about view page.

--- portfolio-website/src/views/AboutView.vue
+++ portfolio-website/src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <CardComp class="mt-2">
+      <template #header>
+        <h2 class="text-xl">About Me:</h2>
+      </template>
+      <template #body>
+        <div class="text-center flex flex-col items-center"> <!-- Center the content within the card's body -->
+          <p>My name is Jonny Vorn Soth. I graduated from Austin High School in 2008. I'm currently learning Vue 3 with
+            JavaScript for the last 6 months. </p>
+        </div>
+      </template>
+      <template #footer>
+        <p>Please contact me at janysoth@gmail.com if you have any questions...</p>
+        <a href="#" class="text-blue-500 hover:underline">Read more</a>
+      </template>
+    </CardComp>
   </div>
 </template>
+
+<script>
+import CardComp from "@/components/ui-library/CardComp.vue";
+
+export default {
+  components: {
+    CardComp,
+  },
+}
+</script>
+
+<style></style>
